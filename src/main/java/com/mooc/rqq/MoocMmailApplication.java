9b7b4b59f7ae4e2c543837c8ff7949866aa230fe
explorren <986554261@qq.com>
2017-09-19 13:10:47 +0800

    first commit

---
+++ src/main/java/com/mooc/rqq/MoocMmailApplication.java
@@ -0,0 +1,12 @@
+package com.mooc.rqq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoocMmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoocMmailApplication.class, args);
+	}
+}
